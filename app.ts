b41c130a72f6b96dbf056033464a03f42c809256
zonde306 <anonymous@localhost>
2025-03-31 04:13:12 +0800

    feat: get models from api

--- app.ts
+++ app.ts
@@ -2,107 +2,11 @@ import { GoogleGenAI, HarmCategory, HarmBlockThreshold, GenerateContentResponse,
 import { Buffer } from "node:buffer";
 import _ from "npm:lodash";
 
-const AVAIABLE_MODELS = {
-    "gemini-2.5-pro-exp-03-25": {
-        id: "gemini-2.5-pro-exp-03-25",
-        object: "model",
-        name: "Gemini 2.5 Pro 实验版",
-        created: Date.now(),
-        owned_by: "google",
-        input: 1048576,
-        output: 65536,
-        rpm: 2,
-        day: 50,
-    },
-    "gemini-2.0-flash": {
-        id: "gemini-2.0-flash",
-        object: "model",
-        name: "Gemini 2.0 Flash",
-        created: Date.now(),
-        owned_by: "google",
-        input: 1048576,
-        output: 8192,
-        rpm: 15,
-        day: 1500,
-    },
-    "gemini-2.0-flash-thinking-exp-1219": {
-        id: "gemini-2.0-flash-thinking-exp-1219",
-        object: "model",
-        name: "	Gemini 2.0 Flash Thinking",
-        created: Date.now(),
-        owned_by: "google",
-        input: 1048576,
-        output: 8192,
-        rpm: 15,
-        day: 1500,
-    },
-    "gemini-2.0-flash-lite": {
-        id: "gemini-2.0-flash-lite",
-        object: "model",
-        name: "Gemini 2.0 Flash-Lite",
-        created: Date.now(),
-        owned_by: "google",
-        input: 1048576,
-        output: 8192,
-        rpm: 30,
-        day: 1500,
-    },
-    "gemini-1.5-flash": {
-        id: "gemini-1.5-flash",
-        object: "model",
-        name: "Gemini 1.5 Flash",
-        created: Date.now(),
-        owned_by: "google",
-        input: 1048576,
-        output: 8192,
-        rpm: 15,
-        day: 1500,
-    },
-    "gemini-1.5-flash-8b": {
-        id: "gemini-1.5-flash-8b",
-        object: "model",
-        name: "Gemini 1.5 Flash-8B",
-        created: Date.now(),
-        owned_by: "google",
-        input: 1048576,
-        output: 8192,
-        rpm: 15,
-        day: 1500,
-    },
-    "gemini-1.5-pro": {
-        id: "gemini-1.5-pro",
-        object: "model",
-        name: "Gemini 1.5 Pro",
-        created: Date.now(),
-        owned_by: "google",
-        input: 2097152,
-        output: 8192,
-        rpm: 2,
-        day: 50,
-    },
-    /*
-    "gemini-embedding-exp": {
-        id: "gemini-embedding-exp",
-        object: "model",
-        name: "Gemini 嵌入",
-        created: Date.now(),
-        owned_by: "google",
-        input: 8192,
-    },
-    "imagen-3.0-generate-002": {
-        id: "imagen-3.0-generate-002",
-        object: "model",
-        name: "Imagen 3",
-        created: Date.now(),
-        owned_by: "google",
-    },
-    */
-};
-
 const MAX_FILE_SIZE = 20 * 1000 * 1000; // 20MB, not Mib
 const API_KEY = Deno.env.get("API_KEY") || "";
 const TOKENS = (Deno.env.get("TOKENS") || "").split(",").map(x => x.trim()).filter(x => x.length > 0);
 const db = await Deno.openKv();
+let MODELS : ModelInfo[] = [];
 
 class ResponseError extends Error {
     constructor(msg : string, opts = {}, ...params: undefined[]) {
@@ -156,45 +60,25 @@ function getTokens(request: Request) : string[] {
     return api_key;
 }
 
-function hash(s: string) {
-    return s.split("").reduce(function(a,b){a=((a<<5)-a)+b.charCodeAt(0);return a&a},0);
-}
-
 async function getBestToken(tokens: string[], model: string, excluding: Set<string> = new Set<string>()) : Promise<string> {
-    // @ts-expect-error: 7053
-    const modelInfo = AVAIABLE_MODELS[model];
+    const models = await fetchModels(tokens[0]);
+    const modelInfo = models.find(x => x.id === model || x.name === model || x.displayName === model);
     if(modelInfo == null) {
         throw new ResponseError(`Invalid model ${model}`, { status: 400 });
     }
 
-    const now = Date.now() / 1000;
-
-    // RPM 和每日限额
-    for(const key of tokens) {
-        const hashedId = hash(key);
-        if(excluding.has(String(hashedId)) || excluding.has(key))
-            continue;
-
-        // 每分钟限额
-        let rpm = ((await db.get([ "gemini", "rpm", model, hashedId ]))?.value || []) as number[];
-        rpm = rpm.filter(x => x > now);
-
-        // 每日限额
-        let rpd = ((await db.get([ "gemini", "rpd", model, hashedId ]))?.value || []) as number[];
-        rpd = rpd.filter(x => x > now);
-
-        if(rpm.length >= modelInfo.rpm || rpd.length >= modelInfo.day) {
-            continue;
-        }
-
-        rpm.push(now + 60);
-        rpd.push(now + 60 * 60 * 24);
-        await db.set([ "gemini", "rpm", model, hashedId ], rpm, { expireIn: 60 * 1000 });
-        await db.set([ "gemini", "rpd", model, hashedId ], rpd, { expireIn: 60 * 60 * 24 * 1000 });
-        return key;
+    function hash(s: string) {
+        return s.split("").reduce(function(a,b){a=((a<<5)-a)+b.charCodeAt(0);return a&a},0);
     }
 
-    throw new ResponseError("No avaiable Key", { status: 400 });
+    const hashedId = hash(tokens.join(","));
+    tokens = tokens.filter(x => !excluding.has(x));
+    if(tokens.length <= 0)
+        throw new ResponseError("No avaiable Key", { status: 400 });
+
+    const poll = ((await db.get([ "gemini", "poll", model, hashedId ]))?.value ?? 0) as number;
+    await db.set([ "gemini", "poll", model, hashedId ], poll + 1, { expireIn: 60 * 60 * 24 * 1000 });
+    return tokens[poll % tokens.length];
 }
 
 interface GeneratePrompt {
@@ -265,10 +149,64 @@ async function formattingMessages(messages: { role: string, content: unknown }[]
     ));
 }
 
-function models() : Response {
+interface ModelInfo {
+    name: string;
+    baseModelId: string;
+    version: string;
+    displayName?: string;
+    description?: string;
+    inputTokenLimit?: number;
+    outputTokenLimit?: number;
+    supportedGenerationMethods?: string[];
+    temperature?: number;
+    maxTemperature?: number;
+    topP?: number;
+    topK?: number;
+    id?: string;
+    input: number;
+    output: number;
+}
+
+async function fetchModels(token: string, cache: boolean = true) : Promise<ModelInfo[]> {
+    if(cache) {
+        if(MODELS.length > 0)
+            return MODELS;
+
+        MODELS = ((await db.get([ "gemini", "models" ]))?.value || []) as ModelInfo[];
+        if(MODELS.length > 0)
+            return MODELS;
+    }
+
+    const response = await fetch(`https://generativelanguage.googleapis.com/v1beta/models?key=${token}&pageSize=1000`);
+    if(!response.ok)
+        return [];
+
+    const data : { models: ModelInfo[] } = await response.json();
+    if(!data.models || data.models.length <= 0)
+        return [];
+
+    MODELS = data.models.map(x => {
+        return {
+            ...x,
+            id: x.baseModelId || x.name.replace(/^models\//, ""),
+            name: x.displayName || x.name,
+            object: "model",
+            created: Date.now(),
+            owned_by: "google",
+            input: x.inputTokenLimit || 4096,
+            output: x.outputTokenLimit || 4096,
+        };
+    }).filter(x => x.supportedGenerationMethods?.includes("generateMessage") || x.supportedGenerationMethods?.includes("generateContent"));
+
+    await db.set([ "gemini", "models" ], MODELS, { expireIn: 7 * 60 * 60 * 24 * 1000 });
+    console.log(MODELS);
+    return MODELS;
+}
+
+async function listModels(tokens: string[]) : Promise<Response> {
     return new Response(JSON.stringify({
         object: "list",
-        data: Object.values(AVAIABLE_MODELS),
+        data: await fetchModels(tokens[0], false),
     }), { status: 200 });
 }
 
@@ -282,9 +220,10 @@ interface GenerateOptions {
     top_k?: number;
 }
 
-function prepareGenerateParams(model: string, prompts: ContentListUnion, options: GenerateOptions = {}) : GenerateContentParameters {
-    // @ts-expect-error: 7053
-    const modelConfig = AVAIABLE_MODELS[model];
+async function prepareGenerateParams(model: string, prompts: ContentListUnion, options: GenerateOptions = {}) : Promise<GenerateContentParameters> {
+    const modelConfig = (await fetchModels("")).find(x => x.id === model || x.name === model || x.displayName === model);
+    if(!modelConfig)
+        throw new ResponseError("Model not found", { status: 404 });
 
     return {
         model: model,
@@ -352,12 +291,14 @@ async function handleStream(ep : GoogleGenAI, model: string, generateParams: Gen
             let promptTokenCount = 0;
             let candidatesTokenCount = 0;
             let cachedContentTokenCount = 0;
+            let lastChunk = null;
 
             // 流式传输
             for await (const chunk of response) {
                 const text = chunk.text;
                 responseId = chunk.responseId || responseId;
                 // console.debug(`stream ${responseId} chunk: ${text}`);
+                lastChunk = chunk;
 
                 if(chunk.candidates?.[0]?.content?.parts?.[0]?.thought) {
                     if(!thinking) {
@@ -418,6 +359,7 @@ async function handleStream(ep : GoogleGenAI, model: string, generateParams: Gen
                     choices: [{
                         index: 0,
                         delta: text ? { content: text } : { role: "assistant" },
+                        finish_reason: chunk?.candidates?.[0]?.finishReason,
                     }],
                     usage: {
                         prompt_tokens: chunk?.usageMetadata?.promptTokenCount,
@@ -437,7 +379,7 @@ async function handleStream(ep : GoogleGenAI, model: string, generateParams: Gen
                 candidatesTokenCount = chunk?.usageMetadata?.candidatesTokenCount || candidatesTokenCount;
                 cachedContentTokenCount = chunk?.usageMetadata?.cachedContentTokenCount || cachedContentTokenCount;
             }
-
+            
             // 结束
             await writer.write(encoder.encode(`data: ${JSON.stringify({
                 id: responseId,
@@ -456,6 +398,9 @@ async function handleStream(ep : GoogleGenAI, model: string, generateParams: Gen
 
             if(totalTokenCount) {
                 console.log(`[output] total ${totalTokenCount} tokens used with model ${model} (input ${promptTokenCount}, output ${candidatesTokenCount}).`);
+                if(!candidatesTokenCount || candidatesTokenCount <= 1) {
+                    console.info(lastChunk);
+                }
             }
         } catch (e) {
             console.error(e);
@@ -500,6 +445,10 @@ async function handleNonStream(ep : GoogleGenAI, model: string, generateParams: 
 
         if(response.usageMetadata?.totalTokenCount) {
             console.log(`[output] total ${response.usageMetadata?.totalTokenCount} tokens used with model ${model} (input ${response.usageMetadata?.promptTokenCount}, output ${response.usageMetadata?.candidatesTokenCount}).`);
+            const candidatesTokenCount = response?.usageMetadata?.candidatesTokenCount;
+            if(!candidatesTokenCount || candidatesTokenCount <= 1) {
+                console.info(response);
+            }
         }
 
         const text = thought + response.text;
@@ -515,7 +464,7 @@ async function handleNonStream(ep : GoogleGenAI, model: string, generateParams: 
                     "role": "assistant",
                     "content": text,
                 },
-                "finish_reason": "stop",
+                "finish_reason": response?.candidates?.[0]?.finishReason || "stop",
             }],
             usage: {
                 prompt_tokens: response?.usageMetadata?.promptTokenCount,
@@ -553,13 +502,12 @@ async function chatCompletions(request: Request, tokens: string[]) : Promise<Res
         const token = await getBestToken(tokens, body.model, excluding);
         const endpoint = new GoogleGenAI({ apiKey: token });
         const prompts = await formattingMessages(body.messages, endpoint);
-        const generateParams = prepareGenerateParams(body.model, prompts, body);
+        const generateParams = await prepareGenerateParams(body.model, prompts, body);
         const counter = await endpoint.models.countTokens(generateParams);
-        console.log(`[input] total ${counter.totalTokens} tokens used with model ${body.model}.`);
-
-        // @ts-expect-error: 7053
-        if(counter.totalTokens && AVAIABLE_MODELS[body.model].input < counter.totalTokens) {
-            throw new ResponseError("too many input tokens", { status: 413 });
+        console.log(`[input] total ${counter.totalTokens} tokens used with model ${body.model} on ${i + 1} times.`);
+        const modelInfo = (await fetchModels(token)).find(x => x.id === body.model || x.name === body.model || x.displayName === body.model);
+        if(!modelInfo || (counter.totalTokens && modelInfo.input && counter.totalTokens > modelInfo.input)) {
+            return new Response(`too may tokens for model ${body.model} (max: ${modelInfo?.input})`, { status: 400 });
         }
 
         try {
@@ -597,7 +545,7 @@ async function handler(request: Request) : Promise<Response> {
         switch(path) {
             case "/v1/models":
             case "/models":
-                return models();
+                return await listModels(tokens);
             case "/v1/chat/completions":
             case "/chat/completions":
                 return await chatCompletions(request, tokens);
